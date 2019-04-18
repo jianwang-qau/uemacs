49de17cf20e0c621710263a4802526ce8797e04c
Jian Wang <jianwang_qau@163.com>
2019-04-18 17:32:46 +0800

    Add syntax highlight for c conditional

--- syntax.c
+++ syntax.c
@@ -22,6 +22,7 @@ static int preprocfg = 0x5FD7FF;	/* preprocess forgrnd color */
 static int typefg = 0x87FFAF;		/* type forgrnd color */
 static int statefg = 0xFCE94F;		/* statement forgrnd color */
 static int labelfg = 0xFCE94F;		/* label forgrnd color */
+static int condfg = 0xFCE94F;		/* conditional forgrnd color */
 
 static char *arr_preproc_if[] = {"if", "ifdef" , "ifndef", NULL};
 static char *arr_preproc_else[] = {"else", "endif", NULL};
@@ -34,6 +35,7 @@ static char *arr_state[] = {
 	"goto", "break", "return", "continue", "asm", NULL
 };
 static char *arr_label[] = {"case", "default", NULL};
+static char *arr_cond[] = {"if", "else", "switch", NULL};
 
 /* syntax highlight flag */
 static int hi_sstring;	/* single line string */
@@ -205,6 +207,8 @@ static void syn_other(struct text *v_text, int vtcol)
 		syn_fcolor(v_text, begin, end, statefg);
 	else if (arr_find(arr_label, synbuf) == TRUE)
 		syn_fcolor(v_text, begin, end, labelfg);
+	else if (arr_find(arr_cond, synbuf) == TRUE)
+		syn_fcolor(v_text, begin, end, condfg);
 }
 
 static void syn_find(struct text *v_text, int begin, int end, int *pbegin, int *pend)
